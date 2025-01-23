1faa5a7655ed42dd591da69de5d6ece68ec3edbf
Devansh Bajpai <devanshb.code@gmail.com>
2025-01-23 12:02:20 +0530

    added basic frontend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const { createServer } = require('node:http');
+const { Server } = require("socket.io");
+const server = createServer(app);
+const io = new Server(server);
+
+
+const path = require('path');
+const cookieParser = require('cookie-parser')
+
+io.on('connection', (socket) => {
+    console.log('a user connected!');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+})
+
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+server.listen(3000, () => {
+    console.log('App is listening on PORT 3000');
+})
